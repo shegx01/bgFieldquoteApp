4b7c9d2ffbe8e6dc6d006e8c7a08829177bbdb28
O.O Samson <shegx01@gmail.com>
2020-05-25 16:07:02 +0100

    added little hover to button and active state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import styles from './header.module.css'
 import {Icon} from '@iconify/react';
 import brainIcon from '@iconify/icons-whh/brain';
@@ -7,17 +7,15 @@ import brainIcon from '@iconify/icons-whh/brain';
 export default function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>
-        <Link to="/">
-          <Icon icon={brainIcon} color='#be7524'/>
-        </Link>
-      </div>
+     <div className={styles.logo}>
+       <Link to="/">
+         <Icon icon={brainIcon} color='#be7524'/>
+       </Link>
+     </div>
 
-      <ul>
-        <li>
-          <Link to="/all-quotes">all quotes</Link>
-        </li>
-      </ul>
+      <nav>
+            <NavLink activeClassName={styles.is_active} className={styles.nav_link} to="/all-quotes">all quotes</NavLink>
+      </nav>
     </div>
   )
 }
